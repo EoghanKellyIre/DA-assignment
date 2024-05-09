20f9e0b8770ba3925190c1781d3f016fee81f31c
EoghanKellyIre <95482796+EoghanKellyIre@users.noreply.github.com>
2024-05-09 17:52:40 +0100

    Add files via upload

--- Mark 11.R
+++ Mark 11.R
@@ -151,13 +151,18 @@ datasetW[] <- lapply(datasetW, function(x) as.numeric(x))
 datasetW$sex <- as.factor(datasetW$sex)
 
 # Resample the data according to the weights
-resampled_samples <- datasetW[sample(nrow(datasetW), size = sum(weights), replace = TRUE, prob = weights), ]
+resampled_samples <- datasetW[sample(nrow(datasetW), size = sum(weights)*100, replace = TRUE, prob = weights), ]
 
 #Defining the DAG
 dag <- model2network("[kappa][lambda][sex][flc.grp|sex:age:lambda:kappa][age][observedDeaths|sex:flc.grp:age]")
 
 # Now fit the network with the resampled data
 bnIPCW <- bn.fit(dag, data = resampled_samples)
+
+# Predict the test data and calculate the mean squared prediction error
+pred <- predict(bnIPCW, node = "observedDeaths", data = resampled_samples)
+mspe <- mean((pred - resampled_samples$observedDeaths)^2)
+mspe
 
 # Create 5 folds
 folds <- createFolds(resampled_samples$observedDeaths, k = 5)
@@ -182,6 +187,11 @@ x
 #IF no weights
 
 bnNoWeights <- bn.fit(dag, data = datasetW)
+
+# Predict the test data and calculate the mean squared prediction error
+pred <- predict(bnNoWeights, node = "observedDeaths", data = datasetW)
+mspe <- mean((pred - datasetW$observedDeaths)^2)
+mspe
 
 # Create 5 folds
 folds <- createFolds(datasetW$observedDeaths, k = 5)
@@ -211,27 +221,10 @@ datasetNoCensored[] <- lapply(datasetNoCensored, function(x) as.numeric(x))
 datasetNoCensored$sex <- as.factor(datasetNoCensored$sex)
 bnNoCensored <- bn.fit(dag, data = datasetNoCensored)
 
-# Create 5 folds
-folds <- createFolds(datasetW$flc.grp, k = 5)
-total_cvmspe <- 0
-
-for (i in 1:length(folds)) {
-  
-  # Split the data into training and test sets
-  train_data <- datasetW[-folds[[i]],]
-  test_data  <- datasetW[folds[[i]],]
-  
-  # Fit the model on the training data
-  bn <- bn.fit(dag, data = train_data)
-  
-  # Predict the test data and calculate the mean squared prediction error
-  pred <- predict(bn, node = "observedDeaths", data = test_data)
-  cvmspe <- mean((pred - test_data$observedDeaths)^2)
-  total_cvmspe <- total_cvmspe + cvmspe
-}
-x<-total_cvmspe/5
-x
-
+# Predict the test data and calculate the mean squared prediction error
+pred <- predict(bnNoCensored, node = "observedDeaths", data = datasetW)
+mspe <- mean((pred - datasetW$observedDeaths)^2)
+mspe
 ########################
 #
 # Plotting
@@ -244,11 +237,11 @@ plot(dag)
 dev.off()
 
 #Plotting curves
-#pdf("kaplan-meier.pdf", width = 10, height = 10)
+pdf("kaplan-meier.pdf", width = 10, height = 10)
 survfit(survivalObj ~ dataset$flc.grp, data = dataset, type="kaplan-meier") %>% 
   ggsurvfit() +
   labs(
     x = "Days",
     y = "Overall survival probability"
   ) + add_confidence_interval()
-#dev.off()
+dev.off()
